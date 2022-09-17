3ca7111fc1be7967628d983b2143c434ffd16451
Udaya Bhasker Mattupalli <mub.oct05@gmail.com>
2022-09-17 14:52:57 -0700

    First Commit sep 17 2022

--- first.js
+++ first.js
@@ -12,3 +12,30 @@ function a() {
   console.log("Bye")
 }
 
+let canada = () => {
+  console.log("arguments", arguments);
+  console.log("Canada");
+}
+
+canada();
+// function india() {
+//   console.log("India");
+// }
+
+
+function india(andhra, delhi) {
+  console.log(arguments, arguments);
+  //console.log("India");
+}
+
+
+india("a", "b");
+
+function india(...args) {
+  console.log(arguments, arguments);
+  console.log(" Array.from(arguments)", Array.from(arguments))
+  //console.log("India");
+}
+
+india("a", "b", "c");
+
